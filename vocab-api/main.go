8a0e0a1e392056db50d7c107b468d0466a0c170c
Ryohskay <learnerryo@gmail.com>
2026-01-07 23:32:51 +0900

    add comments

--- vocab-api/main.go
+++ vocab-api/main.go
@@ -9,6 +9,7 @@ func main() {
 	router := gin.Default()
 	db, ctx := dbaccess.DbConnInit()
 	router.GET("api/languages", ListLangs(db, ctx))
+	// define path param `lang_id` (accessible via c.Param())
 	router.GET("api/:lang_id/vocabulary", ListVocabs())
 	router.Run()
 }
