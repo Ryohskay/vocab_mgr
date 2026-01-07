6211982adfa5c170fa1de415e52e855f986d0446
Ryohskay <learnerryo@gmail.com>
2026-01-07 21:15:42 +0900

    DbConnInit in a separate func

--- vocab-api/dbaccess/dbaccess.go
+++ vocab-api/dbaccess/dbaccess.go
@@ -18,7 +18,9 @@ type Language struct {
 	Area_used string
 }
 
-func GetLangs() ([]Language, error) {
+func DbConnInit() (*gorm.DB, context.Context) {
+	// initialise the DB connection
+	// return the initialised context
 	db, err := gorm.Open(sqlite.Open("../data/database.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatalln("Failed to connect!", err)
@@ -26,6 +28,11 @@ func GetLangs() ([]Language, error) {
 	ctx := context.Background()
 
 	db.AutoMigrate(&Language{})
+
+	return db, ctx
+}
+
+func GetLangs(db *gorm.DB, ctx context.Context) ([]Language, error) {
 	// err = gorm.G[Language](db).Create(ctx, &Language{ISO: "xba", Script: "Bhks", Endonym: "Mattam bhasa", Exonym_en: "Martabanese", Area_used: "Southeast Asia"})
 
 	langs, err := gorm.G[Language](db).Find(ctx)
--- vocab-api/main.go
+++ vocab-api/main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+	"context"
+	"gorm.io/gorm"
 	"github.com/gin-gonic/gin"
 	"github.com/Ryohskay/vocab_mgr/vocab-api/dbaccess"
 )
 
 func main() {
 	router := gin.Default()
-	router.GET("api/languages", listLangs)
+	db, ctx := dbaccess.DbConnInit()
+	router.GET("api/languages", listLangs(db, ctx))
 	router.Run()
 }
 
-func listLangs(c *gin.Context) {
+func listLangs(db *gorm.DB, ctx context.Context) gin.HandlerFunc {
+	return func (c *gin.Context) {
 	
-	langs, err := dbaccess.GetLangs()
-	if err == nil {
-		c.JSON(200, langs)
-	} else {
-		c.JSON(500, gin.H{
-			"message": "Failed to get languages from the DB",
-		})
+		langs, err := dbaccess.GetLangs(db, ctx)
+		if err == nil {
+			c.JSON(200, langs)
+		} else {
+			c.JSON(500, gin.H{
+				"message": "Failed to get languages from the DB",
+			})
+		}
 	}
 }
