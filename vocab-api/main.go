8c71c91b2da81a89cc477e2319d7fb642395c115
Ryohskay <learnerryo@gmail.com>
2026-01-07 21:51:18 +0900

    move the handler func to handlers.go

--- vocab-api/main.go
+++ vocab-api/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"context"
-	"gorm.io/gorm"
 	"github.com/gin-gonic/gin"
 	"github.com/Ryohskay/vocab_mgr/vocab-api/dbaccess"
 )
@@ -10,20 +8,6 @@ import (
 func main() {
 	router := gin.Default()
 	db, ctx := dbaccess.DbConnInit()
-	router.GET("api/languages", listLangs(db, ctx))
+	router.GET("api/languages", ListLangs(db, ctx))
 	router.Run()
-}
-
-func listLangs(db *gorm.DB, ctx context.Context) gin.HandlerFunc {
-	return func (c *gin.Context) {
-	
-		langs, err := dbaccess.GetLangs(db, ctx)
-		if err == nil {
-			c.JSON(200, langs)
-		} else {
-			c.JSON(500, gin.H{
-				"message": "Failed to get languages from the DB",
-			})
-		}
-	}
 }
